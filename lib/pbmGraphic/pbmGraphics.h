b249202120d5c9d0a1ce38cd541cddb61c18deb1
Adrian STEINER <steia19@bfh.ch>
2025-06-14 00:23:01 +0200

    Update doxygen comments, fix pbm fill error

--- lib/pbmGraphic/pbmGraphics.h
+++ lib/pbmGraphic/pbmGraphics.h
@@ -1,11 +1,29 @@
 /**
  * @file pbmGraphics.h
  * @author Adrian STEINER (adi.steiner@hotmail.ch)
- * @brief Graphic library to set in an PBM image pixels, lines and writing to it.
- * @version 0.1
- * @date 19-12-2024
+ * @brief Graphic library to set in an PBM image (1 pixel depth image) pixels, lines and writing to it.
+ *        This header file defines the function prototypes and ads the necessary pbmFontHandler
+ *        for writing text to the image.
  *
- * @copyright Copyright (c) 2024
+ * This lib is used write graphical (pixels, lines) elements and write text in the image.
+ *
+ * @version 0.2
+ * @date 19-12-2024
+ * @date 13-06-2025
+ *
+ * @copyright (C) 2025 Adrian STEINER
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https: //www.gnu.org/licenses/>.
  *
  */
 
